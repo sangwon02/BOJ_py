3d7ae3a4c9e8aa67cd70977b73ed71c1bd2c8f89
sangwon02 <sangwon020214@gmail.com>
2022-01-22 23:22:40 +0900

    백준 9일차

--- ex56.py
+++ ex56.py
@@ -1,2 +1,12 @@
- 
-s
+n = int(input())
+
+for i in range(n):
+    num = []
+    a = int(input())
+    b = int(input())
+    num = [j for j in range(1, b+1)]
+
+    for l in range(a):
+        for j2 in range(1, b):
+            num[j2] += num[j2-1]
+    print(num[b-1])
