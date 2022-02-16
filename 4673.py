84f02e5c4c2328086b6dd0bb72f75f84401ccd3f
sangwon02 <sangwon020214@gmail.com>
2022-02-16 23:45:59 +0900

    Solved: BOJ 4673 셀프 넘버

---
+++ 4673.py
@@ -0,0 +1,15 @@
+#꼭 다시 보가
+#4673 셀프넘버
+def d(n):
+    x = 0
+    a = list(str(n))
+    for i in a:
+        x = x + int(i)
+    return n+x
+
+s_set = set()
+for k in range(1,10000):
+    s_set.add(d(k))
+ans = set(range(1,10000)) - s_set
+for num in sorted(ans):
+    print(num)
