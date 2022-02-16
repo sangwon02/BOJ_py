ad2a653366a145f940af61122593cc84317a37e8
sangwon02 <sangwon020214@gmail.com>
2022-02-16 23:45:59 +0900

    Solved: BOJ 8958 OX퀴즈

---
+++ 8958.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+for i in range(n):
+    a = input()
+    score = 0
+    sum = 0
+    for j in a:
+        if j == 'O':
+            score += 1
+        else:
+            score = 0
+        sum += score
+    print(sum)
