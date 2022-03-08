a62aa1086a3068a921a0c97183e3bec17159b79f
sangwon02 <sangwon020214@gmail.com>
2022-03-08 23:37:05 +0900

    Solved: BOJ 5361 전투 드로이드 가격

---
+++ 5361.py
@@ -0,0 +1,10 @@
+n = int(input())
+li = []
+
+for i in range(n):
+    n1, n2, n3, n4, n5 = map(int, input().split())
+    li.append(n1*350.34+n2*230.90+n3*190.55+n4*125.30+n5*180.90) 
+
+for j in li:
+    print("$",round(j,2))
+
