cd32fa9525934b24ef07f920a32f2ed2db26ba30
sangwon02 <sangwon020214@gmail.com>
2022-03-16 00:04:54 +0900

    기타 파일 추가/수정

--- ex.py
+++ ex.py
@@ -1,9 +1,19 @@
+i = 0
+
 n = int(input())
+
 str = input()
+
 n1, n2 = map(int, input().split())
 
-car_list = list(map(int, input().split()))
+print(i, end ="")
+
+li = list(map(int, input().split()))
+
+print("dfsfsdfsdfsdf"[:3])
+#3번째 까지 문자열 출력
 
 li =[]
+
 for i in range(n):
     li.append(int(input()))
