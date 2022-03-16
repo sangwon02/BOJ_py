12eed9175a99897e59187e8d8051da309165b442
sangwon02 <sangwon020214@gmail.com>
2022-03-16 11:09:49 +0900

    Solved: BOJ 14910 오르막

--- 14910.py
+++ 14910.py
@@ -1,12 +1,6 @@
 li = list(map(int, input().split()))
-if len(li) == 1:
-    print("Good")
-    exit()
-    
-for i in range(len(li)):
-    
-    if li[i] > li[i+1]:
-        print("Bad")
-        exit()
 
-print("Good")
+if li == sorted(li): 
+    print("Good")
+else: 
+    print("Bad")
