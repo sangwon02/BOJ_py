ec0ff34bd470ad441bc00808e81d2796bebd12b7
sangwon02 <sangwon020214@gmail.com>
2022-03-30 23:40:29 +0900

    Solved: BOJ 9226 도깨비말

--- 9226.py
+++ 9226.py
@@ -1,4 +1,10 @@
+str2 =""
+str1 =input()
 while str1 != "#":
     str1 = input()
-    for i in str1:
-        if 
+    for i in range(len(str1)):
+        if str1[i] == 'a' or str1[i] == 'e' or str1[i] == 'i' or str1[i] == 'o' or str1[i] == 'u':
+            print(str1[i:],str2,"ay")
+            continue
+        else:
+            str2 = str2 + str1[i]
