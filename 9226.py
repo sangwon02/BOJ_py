12c219f3080e4654a1c0b55a51965981ea7abf33
sangwon02 <sangwon020214@gmail.com>
2022-03-31 23:35:03 +0900

    Solved: BOJ 9226 도깨비말

--- 9226.py
+++ 9226.py
@@ -1,10 +1,19 @@
-str2 =""
-str1 =input()
-while str1 != "#":
-    str1 = input()
-    for i in range(len(str1)):
-        if str1[i] == 'a' or str1[i] == 'e' or str1[i] == 'i' or str1[i] == 'o' or str1[i] == 'u':
-            print(str1[i:],str2,"ay")
-            continue
-        else:
-            str2 = str2 + str1[i]
+vowel = ['a','e','i','o','u']
+while 1:
+    s = input()
+    if s=='#' : 
+        break
+    li = []
+    for i in s: 
+        li.append(i)
+
+    if 'a' not in li and 'e' not in li and 'i' not in li and 'o' not in li and 'u' not in li:
+        print(s+"ay")
+    
+    else:
+        while li[0] not in vowel :
+            li.append(li[0])
+            del li[0]
+        for i in li:
+            print(i,end='')
+        print("ay")
