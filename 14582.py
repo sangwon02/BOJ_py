28ac64daa36e2ceb99fa825a44181bc151da5e33
sangwon02 <sangwon020214@gmail.com>
2022-04-07 00:23:01 +0900

    백준 푼 것

--- 14582.py
+++ 14582.py
@@ -1,17 +1,18 @@
 attack_list = list(map(int, input().split()))
 defense_list = list(map(int, input().split()))
 
-result = [0]
-
 attack_num = 0
 defense_num = 0
 
-prin = ""
+check = False
 
 for i in range(9):
     attack_num += attack_list[i]
+    if attack_num > defense_num:
+        check = True
     defense_num += defense_list[i]
 
-    result.append(defense_num - attack_num)
-
-    if result 
+if attack_num < defense_num and check:
+    print("Yes")
+else:
+    print("No")
