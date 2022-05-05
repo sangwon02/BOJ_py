b228518eb9312889ef66a43c430c1a7e5d2f24dc
sangwon02 <sangwon020214@gmail.com>
2022-05-05 22:29:53 +0900

    백준 푼 것

--- 2562.py
+++ 2562.py
@@ -1,11 +1,11 @@
-num = []
+num = [] #숫자를 저장할 리스트
 
-for i in range(9):
-    a = int(input())
-    num.append(a)
-a = num[0]
-for n in num:
-    if n >= a:
-        a = n
-b = int(num.index(a))
-print(a, b+1)
+for i in range(9): #9번 반복
+    a = int(input()) #숫자를 입력 받음
+    num.append(a) #입력받은 숫자를 리스트에 저장함
+a = num[0] #첫번째 리스트의 값을 a에 저장
+for n in num: #리스트에 있는 요소의 수만큼 반복(n에 그 순서의 리스트값 할당)
+    if n >= a: #만약 n이 a보다 크다면
+        a = n #a에 n을 저장
+b = int(num.index(a)) #b에 a의 인덱스값을 찾아 저장 
+print(a, b+1) #a와 b+1을 출력
--- 2577.py
+++ 2577.py
@@ -1,17 +1,21 @@
-a = int(input())
+a = int(input()) #3개의 숫자를 각각 a, b, c에 입력받음
 b = int(input())
 c = int(input())
-x = a*b*c
-n1 = 10
-n3 = 1
+x = a*b*c #a, b, c를 곱한 값을 x에 저장
+n1 = 10 
+n3 = 1 
 list = [0,0,0,0,0,0,0,0,0,0,0]
 
-while True:
-    n2 = (x%n1)//n3
-    list[n2]=list[n2]+1
-    n1 = n1*10
+while True: #무한 반복
+    n2 = (x%n1)//n3 #n2에 (x%n1)//n3를 저장해준다.
+    #1번째 시도하면 마지막 첫째자리의 숫자
+    #2번째 시도하면 마지막 둘째자리의 숫자
+    #3번째 시도하면 마지막 셋째자리의 숫자
+    #.....
+    list[n2]=list[n2]+1 #n2가 있는 위치에 +1
+    n1 = n1*10 #각각의 자리수의 숫자를 찾기위해 10씩 곱해준다.
     n3 = n3*10
-    if n1>x*10:
-        break
-for i in range(10):
+    if n3>x: #만약 n3의 값이 x보다 작으면 
+        break #while문 종료
+for i in range(10): #각 리스트의 값을 출력
     print(list[i])
