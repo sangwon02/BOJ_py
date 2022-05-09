74df8806b4c28f2d2f1ad55f95be86a3af3967ef
sangwon02 <sangwon020214@gmail.com>
2022-05-09 23:12:00 +0900

    Solved: BOJ 3062 수 뒤집기

--- 3062.py
+++ 3062.py
@@ -1,10 +1,10 @@
-n = int(input())
+n = int(input()) #반복할 숫자 입력받음
 
-for i in range(n):
-    a = int(input())
-    b = int(str(a)[::-1])
-    c = a + b
-    if c == int(str(c)[::-1]):
-        print("YES")
-    else:
-        print("NO")
+for i in range(n):#n만큼 반복
+    a = int(input()) #숫자 입력 받음
+    b = int(str(a)[::-1]) #입력 받은 숫자 뒤집어줌
+    c = a + b #a하고 b 더함
+    if c == int(str(c)[::-1]): #c와 거꾸로 된 c가 같으면
+        print("YES") #YES 출력
+    else: #다르면
+        print("NO") #NO 출력
