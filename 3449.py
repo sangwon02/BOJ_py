232f44dc39d7db5d70500cb33c0692cd54f8608f
sangwon02 <sangwon020214@gmail.com>
2022-05-10 23:09:30 +0900

    백준 푼 것

--- 3449.py
+++ 3449.py
@@ -1,10 +1,10 @@
-n = int(input())
+n = int(input()) #테스트 케이스의 개수 n
 
-for i in range(n):
-    str1 = input()
-    str2 = input()
-    cnt = 0
-    for j in range(len(str1)):
-        if str1[j] != str2[j]:
-            cnt += 1
-    print("Hamming distance is %d."%cnt)
+for i in range(n): #n만큼 반복
+    str1 = input() #첫번째 숫자 입력 받음
+    str2 = input() #두번째 숫자 입력 받음
+    cnt = 0 #다른 개수 새주는 변수
+    for j in range(len(str1)):#숫자의 자릿수만큼 반복
+        if str1[j] != str2[j]: #만약 같은 위치의 숫자가 다르면
+            cnt += 1 #cnt += 1
+    print("Hamming distance is %d."%cnt)#해밍 거리 출력
--- 4435.py
+++ 4435.py
@@ -1,13 +1,16 @@
-n = int(input())
+n = int(input()) #전투 횟수 입력받음
 
-for i in range(n):
-    li1 = list(map(int, input().split()))
+for i in range(n): #n만큼 반복
+    #공백을 구분하여 정수 입력받음
+    li1 = list(map(int, input().split())) 
     li2 = list(map(int, input().split()))
-    sum1 = li1[0] + li1[1]*2 + li1[2]*3 + li1[3]*3 + li1[4]*4 + li1[5]*10
+    #각각의 종족의 따라 점수를 구해 더해준다
+    sum1 = li1[0] + li1[1]*2 + li1[2]*3 + li1[3]*3 + li1[4]*4 + li1[5]*10 
     sum2 = li2[0] + li2[1]*2 + li2[2]*2 + li2[3]*2 + li2[4]*3 + li2[5]*5 + li2[6]*10
-    if sum1 > sum2:
-        print("Battle %d: Good triumphs over Evil"%(i+1)) 
-    elif sum1 < sum2:
-        print("Battle %d: Evil eradicates all trace of Good"%(i+1)) 
-    else:
-        print("Battle %d: No victor on this battle field"%(i+1)) 
+    #각 전투에 대해 전투 번호를 함께 출력한다 
+    if sum1 > sum2:#간달프의 군대가 이기면
+        print("Battle %d: Good triumphs over Evil"%(i+1)) #간달프의 군대가 이겼다고 출력
+    elif sum1 < sum2: #사우론의 군대가 이기면
+        print("Battle %d: Evil eradicates all trace of Good"%(i+1)) #사우론의 군대가 이겼다고 출력
+    else: #비기면
+        print("Battle %d: No victor on this battle field"%(i+1))  #비겼다고 출력
