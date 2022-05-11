5f4169077538250e30c701a72dd632f24fea622c
sangwon02 <sangwon020214@gmail.com>
2022-05-11 23:11:23 +0900

    백준 푼 것

--- 4458.py
+++ 4458.py
@@ -1,6 +1,6 @@
-N = int(input())
+N = int(input()) #입력 받을 문자의 수
 
-for i in range(N):
-    s = str(input())
-    s= s[0].upper()+s[1:]
-    print(s)
+for i in range(N): #N만큼 반복
+    s = str(input()) #문자를 입력 받음
+    s= s[0].upper()+s[1:] #첫글자를 대문자로 바꿔주고 나머지 글자는 그대로
+    print(s) #s를 출력
