c0de36e60c2234d374c5b6454cd4fdfd474b49da
sangwon02 <sangwon020214@gmail.com>
2022-05-11 23:11:23 +0900

    Solved: BOJ 5586 JOI와 IOI

--- 5586.py
+++ 5586.py
@@ -1,10 +1,10 @@
-str1 = input()
-cnt1 = 0
-cnt2 = 0
-for i in range(len(str1)-2):
-    if str1[i:i+3] == "JOI":
-        cnt1 += 1
-    elif str1[i:i+3] == "IOI":
-        cnt2 += 1
-print(cnt1)
-print(cnt2)
+str1 = input() #문자를 입력받음
+cnt1 = 0 #JOI횟수 셀 변수
+cnt2 = 0 #IOI횟수 셀 변수
+for i in range(len(str1)-2): #문자릐 길이보다 2작은 수만큼 반복
+    if str1[i:i+3] == "JOI": #i번째 문자부터 i+3문자가 JOI면
+        cnt1 += 1 #cnt1 +1
+    elif str1[i:i+3] == "IOI": #i번째 문자부터 i+3문자가 IOI면
+        cnt2 += 1 #cnt2 +1
+print(cnt1) #cnt1 출력
+print(cnt2) #cnt2 출력
