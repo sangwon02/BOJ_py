8ff0e9fbb2467cfa6225cee8adacf7452245e1e1
sangwon02 <sangwon020214@gmail.com>
2022-05-12 22:26:00 +0900

    백준 푼 것

--- 5622.py
+++ 5622.py
@@ -1,22 +1,22 @@
-st = input().upper()
-num = 0
-for i in st:
-    if i == 'B'or i == 'C'or i == 'A':
-        num +=3
-    elif i == 'E'or i == 'F'or i == 'D':
-        num +=4
-    elif i == 'H'or i == 'I'or i == 'G':
-        num +=5
-    elif i == 'K'or i == 'J'or i == 'L':
-        num +=6
-    elif i == 'N'or i == 'O'or i == 'M':
-        num +=7
-    elif i == 'Q'or i == 'S'or i == 'R'or i == 'P':
-        num +=8
-    elif i == 'U'or i == 'V'or i == 'T':
-        num +=9
-    elif i == 'X'or i == 'Y'or i == 'W'or i == 'Z':
-        num +=10
-    else:
-        num +=11
-print(num)
+st = input().upper() #문자열을 입력받음
+sum = 0 #시간의 합
+for i in st: #문자열의 문자를 순서대로 i에 대입 후 밑 실행
+    if i == 'B'or i == 'C'or i == 'A': #i가 B,C,A이면
+        sum +=3 #3을 sum에 더함
+    elif i == 'E'or i == 'F'or i == 'D':#i가 E,F,D이면
+        sum +=4 #4을 sum에 더함
+    elif i == 'H'or i == 'I'or i == 'G':#i가 H,I,G이면
+        sum +=5 #5을 sum에 더함
+    elif i == 'K'or i == 'J'or i == 'L':#i가 K,J,L이면
+        sum +=6 #6을 sum에 더함
+    elif i == 'N'or i == 'O'or i == 'M':#i가 N,Q,M이면
+        sum +=7 #7을 sum에 더함
+    elif i == 'Q'or i == 'S'or i == 'R'or i == 'P':#i기 Q,S,R이면
+        sum +=8 #8을 sum에 더함
+    elif i == 'U'or i == 'V'or i == 'T':#i가 U,V,T이면
+        sum +=9 #9을 sum에 더함
+    elif i == 'X'or i == 'Y'or i == 'W'or i == 'Z':#i가 X,Y,W,Z이면
+        sum +=10 #10을 sum에 더함
+    else:#나머지 경우면
+        sum +=11 #11을 sum에 더함
+print(sum) #sum을 출력
--- 5656.py
+++ 5656.py
@@ -1,9 +1,12 @@
-cnt=1
-while True:
-    n1,s,n2=input().strip().split()
-    if s=="E":
-        break
-    if s=="!=":
+cnt=1 #카운트할 변수
+while True: #무한 반복
+    n1,s,n2=input().strip().split()#띄어쓰기를 기준으로 3개의 문자를 입력받음
+    if s=="E": #s가 E이면
+        break #종료
+    #각각의 식이 맞는지 아닌지 확인한다.
+    #만약 아니라면 false출력 맞다면 true출력
+    if s=="!=": 
+        #format(cnt)는 {}에 넣어줌
         print("Case {}: ".format(cnt)+str(int(n1)!=int(n2)).lower())
     elif s=="<":
         print("Case {}: ".format(cnt)+str(int(n1)<int(n2)).lower())
@@ -15,4 +18,4 @@ while True:
         print("Case {}: ".format(cnt)+str(int(n1)>=int(n2)).lower())
     elif s=="==":
         print("Case {}: ".format(cnt)+str(int(n1)==int(n2)).lower())
-    cnt+=1
+    cnt+=1 #cnt에 1를 더해줌
