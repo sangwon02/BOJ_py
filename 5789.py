dbc907ffc1124a49f075ed2d756d36d112ae7492
sangwon02 <sangwon020214@gmail.com>
2022-05-13 22:43:58 +0900

    Solved: BOJ 5789 한다 안한다

--- 5789.py
+++ 5789.py
@@ -1,8 +1,8 @@
-N = int(input())
+N = int(input()) #반복할 수를 입력 받음
 
-for i in range(N):
-    s = input()
-    if s[len(s)//2] == s[len(s)//2 -1]:  
-        print("Do-it")
-    else: 
-        print("Do-it-Not")
+for i in range(N): #N만큼 반복
+    s = input() #문자열을 입력받음
+    if s[len(s)//2] == s[len(s)//2 -1]: #중심부분의 문자가 같으면
+        print("Do-it") #Do-it 출력
+    else: #다르면
+        print("Do-it-Not")#Do-it-Not출력
