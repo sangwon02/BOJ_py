f77735ce73347a3d35cb3d72fcb329f132cfae5a
sangwon02 <sangwon020214@gmail.com>
2022-05-14 22:36:35 +0900

    백준 푼 것

--- 9086.py
+++ 9086.py
@@ -1,5 +1,5 @@
-T = int(input())
+T = int(input()) #반복할 숫자 입력받음
 
-for i in range(T):
-    s = input()
-    print(s[0]+s[-1])
+for i in range(T): #T만큼 반복
+    s = input() #문자열을 입력받음
+    print(s[0]+s[-1]) #문자열의 첫번째 문자와 마지막 문자를 출력한다.
