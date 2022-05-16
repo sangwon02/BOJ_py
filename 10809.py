f0d17aaa1fdf01904d7305111ba13c125896c394
sangwon02 <sangwon020214@gmail.com>
2022-05-16 19:12:32 +0900

    Solved: BOJ 10809 알파벳 찾기

--- 10809.py
+++ 10809.py
@@ -1,5 +1,5 @@
-st = input()
-alpha = list(range(97,123))
+st = input() #문자열을 입력 받음
+alpha = list(range(97,123)) #리스트에 a부터 z까지의 아스키 코드 추가 
 
-for x in alpha :
-    print(st.find(chr(x))) 
+for x in alpha : #x가 alpha(리스트)의 문자를 대입하며 반복
+    print(st.find(chr(x))) #문자가 각 아스키코드에 대응하는 문자가 몇개 있는지 출력
