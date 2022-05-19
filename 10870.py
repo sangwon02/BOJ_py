565c66b03df90ce9a1c7a23622283dea46d5923f
sangwon02 <sangwon020214@gmail.com>
2022-05-19 23:19:19 +0900

    백준 푼 것

--- 10870.py
+++ 10870.py
@@ -1,9 +1,9 @@
-def fun(n):
-    if n <= 1:
-        return n
+def fun(n): #피보나치 수 리턴 함수
+    if n <= 1: #n이 1보다 작거나 같으면 
+        return n #리턴 n
 
-    return fun(n-1) + fun(n-2)
+    return fun(n-1) + fun(n-2) #Fn = Fn-1 + Fn-2 (n ≥ 2) 피보나치 수 리턴
 
-n = int(input())
+n = int(input()) #숫자 입력 받음
 
-print(fun(n))
+print(fun(n)) #피보나치 수 프린트
--- 11720.py
+++ 11720.py
@@ -1,14 +1,8 @@
-n = int(input())
-n1 = int(input())
-n5 = 0
-n3 = 10
-n4 = 1
+n = int(input())#입력 받을 숫자의 자리수
+n1 = input() #숫자 입력 받음
+sum1 = 0 #합을 저장할 변수
 
-for i in range(n):
-    n2 = (n1%n3)//n4
-    n3 = n3*10
-    n4 = n4*10
-    n5 = n5+n2
+for i in n1: #n1의 문자열을 i에 대입하면서 반복
+    sum1 += int(i) #sum1에 i를 더함
 
-print(n5)
-
+print(sum1) #출력
