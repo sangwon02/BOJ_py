92bf9467b3e82e25d3e1664dfd6d90e76530a2fa
sangwon02 <sangwon020214@gmail.com>
2022-05-20 23:17:44 +0900

    Solved: BOJ 11944 NN

--- 11944.py
+++ 11944.py
@@ -1,3 +1,4 @@
-n1, n2 = map(int, input().split())
+n1, n2 = map(int, input().split()) #숫자 두개를 공백을 구분하여 입력 받음
 
-print((str(n1)*n1)[:n2])
+print((str(n1)*n1)[:n2]) #n1을 n1번 출력한다. 
+#만약 답이 길어지면 답의 앞 n2자리를 출력
