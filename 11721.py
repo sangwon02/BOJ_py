f87de63273e27568b7a502eeec765ceda651c19e
sangwon02 <sangwon020214@gmail.com>
2022-05-20 23:17:44 +0900

    백준 푼 것

--- 11721.py
+++ 11721.py
@@ -1,7 +1,7 @@
-str1 = input()
-cnt = 0
-for i in str1:
-    print(i, end ="")
-    cnt += 1
-    if cnt%10==0:
-        print(" ")
+str1 = input() #문자입력 받음
+cnt = 0 #카운트 해줄 변수
+for i in str1: #입력받은 문자열의 문자를 i에 하나씩 대입하면서 반복
+    print(i, end ="") #i프린트(줄바꿈 없이)
+    cnt += 1 #cnt +1
+    if cnt%10==0: #cnt가 10으로 나누어지면
+        print(" ") #줄바꿈
