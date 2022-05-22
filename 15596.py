0c0fae469c0e0bd9364e5a4da5f38f27e26c6005
sangwon02 <sangwon020214@gmail.com>
2022-05-22 23:07:24 +0900

    Solved: BOJ 15596 정수 N개의 합

--- 15596.py
+++ 15596.py
@@ -1,7 +1,7 @@
-def solve(a):
-    result = 0
-    for num in a:
-        result += num
-    return result
+def solve(a): #함수 작성
+    result = 0 #변수 선언
+    for num in a: #a(함수 input값 (리스트))에 있는 원소를 num에 차례로 대입하면서 반복
+        result += num #num의 값을 result 더함
+    return result #리턴 result
 
 
