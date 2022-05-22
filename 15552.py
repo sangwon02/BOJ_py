9a556dc34177cd503e2e065b4f835ef095c88ded
sangwon02 <sangwon020214@gmail.com>
2022-05-22 23:07:24 +0900

    백준 푼 것

--- 15552.py
+++ 15552.py
@@ -1,7 +1,7 @@
-import sys
+import sys #sys를 불러옴
 
-x = int(input())
+x = int(input()) #정수를 입력 받음
 
-for _ in range(x):
-    y, z = map(int, sys.stdin.readline().split())
-    print(y+z)
+for _ in range(x): #x만큼 반복
+    y, z = map(int, sys.stdin.readline().split()) #공백을 구분하여 정수 두개를 받음
+    print(y+z) # y+z 출력
