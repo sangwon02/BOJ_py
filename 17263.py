93d40f941a2016fa41ee406ffc08c00189ca532d
sangwon02 <sangwon020214@gmail.com>
2022-05-23 21:12:40 +0900

    백준 푼 것

--- 17263.py
+++ 17263.py
@@ -1,7 +1,7 @@
-n = int(input())
+n = int(input()) #문자를 넣을 정수 입력 받음
 
-li = list(map(int, input().split()))
+li = list(map(int, input().split())) #공백을 구분하여 정수를 리스트에 입력 받음
 
-li.sort(reverse=False)
+li.sort(reverse=False) #리스트르 오름차순으로 정렬한다
 
-print(li[n-1])
+print(li[n-1]) #리스트의 마지막 원소 출력
