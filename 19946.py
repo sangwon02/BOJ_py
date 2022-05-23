13b35a92473dc9d0d2c4306709dcfb9767ae0504
sangwon02 <sangwon020214@gmail.com>
2022-05-24 00:12:22 +0900

    백준 푼 것

--- 19946.py
+++ 19946.py
@@ -1,8 +1,8 @@
-N = int(input())
-cnt = 64
+N = int(input()) #정수를 받음
+cnt = 64 #실수한 구간을 확인해줄 변수
 
-while N % 2 == 0:
-    N //= 2
-    cnt -= 1
+while N % 2 == 0: #N이 2로 나누어지면 반복
+    N //= 2 #N을 2로 나누어줌
+    cnt -= 1 #cnt에 -1를 해줌
 
-print(cnt)
+print(cnt) #실수한 구간 출력
