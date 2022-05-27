e1e511f8ab3e41076fcd7ddaa5014f1ae422e3a1
sangwon02 <sangwon020214@gmail.com>
2022-05-27 23:28:17 +0900

    Solved: BOJ 2750 수 정렬하기

--- 2750.py
+++ 2750.py
@@ -1,10 +1,11 @@
-n = int(input())
-li = []
+n = int(input()) #입력 받을 숫자의 개수를 입력 받음
+li = [] #리스트 생성
+ 
+for i in range(n): #n만큼 반복
+    li.append(int(input())) #정수를 입력 받아 리스트에 넣어줌
 
-for i in range(n):
-    li.append(int(input()))
+li.sort(reverse=False) #리스트를 오름차순으로 정렬
 
-li.sort(reverse=False)
-
-for i in li:
+#리스트 출력
+for i in li: 
     print(i)
