6772e4f4945a69ae00ce305feaa4d990b9b4f08f
sangwon02 <sangwon020214@gmail.com>
2022-05-28 18:00:31 +0900

    백준 푼 것

--- 1193.py
+++ 1193.py
@@ -1,15 +1,15 @@
-n = int(input())
+n = int(input()) #정수를 입력 받음
 num1 = 1
 
-while n>num1:
-    n-=num1
-    num1+=1
+while n>num1: #n이 num1보다 크면
+    n -= num1 #n은 n-num1을 해주고 
+    num1 += 1 #num1에 1을 더해준다
 
-if num1%2==0:
-    a=n 
-    b=num1-n+1
+if num1%2==0: #num1이 2로 나누어지면
+    a=n #a는 분자 부분
+    b=num1-n+1 #b는 분모
     
-else:
-    a=num1-n+1
-    b=n
-print(a, '/', b, sep='')
+else: #num1이 2로 나누어지지 않으면
+    a=num1-n+1 #a는 분자 부분
+    b=n #b는 분모
+print(a, '/', b, sep='') #분수 출력
--- 2869.py
+++ 2869.py
@@ -1,8 +1,8 @@
-a, b, v= map(int, input().split())
-k = (v-b)/(a-b)
+a, b, v= map(int, input().split()) #공백을 구분하여 정수를 3개 입력 받음
+k = (v-b)/(a-b) #남은 거리와 하루에 올라는 거리를 나누어주어 저장함 
 
-if k == int(k):
-    print(int(k))
-else:
-    print(int(k)+1)
+if k == int(k): #k가 정수면
+    print(int(k)) #k만 프린트
+else: #정수가 아니면
+    print(int(k)+1) #k를 반올림 해주어 정수로 출력
  
