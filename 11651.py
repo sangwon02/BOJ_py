d3fa3f080976f5cee6610f8f17e444092015f5f9
sangwon02 <sangwon020214@gmail.com>
2024-04-01 22:05:30 +0900

    백준 푼 것

---
+++ 11651.py
@@ -0,0 +1,12 @@
+import sys
+n=int(sys.stdin.readline())
+li=[]
+
+for i in range(n):  # 좌표값 입력 받고
+    x, y = map(int, sys.stdin.readline().split())
+    li.append([y, x])  # 리스트에 넣을때는 반대로
+    
+li = sorted(li)  # 좌표 정렬
+
+for y, x in li:  # 좌표 출력
+    print(x, y)
