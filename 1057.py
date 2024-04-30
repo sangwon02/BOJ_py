968a7d6ba1590d5d5245f8dcc8923f9351b9956f
sangwon02 <sangwon020214@gmail.com>
2024-04-30 17:06:17 +0900

    Solved: BOJ 1057 토너먼트

---
+++ 1057.py
@@ -0,0 +1,17 @@
+import sys
+import math
+input = sys.stdin.readline
+
+n, k, l = map(int, input().split())  # 각 선수 수와 지민, 한수의 번호 입력 받음
+cnt = 1  # 몇 라운드인지 카운트 해줄 변수
+while True:
+    n = math.ceil(n/2)  # 라운드가 지날때 마다 자기번호/2 해준 후 올림했을때
+    k = math.ceil(k/2)  # 같은 번호 끼리 만남
+    l = math.ceil(l/2)
+    if k == l:  # 만약 지민이와 한수가 만나면 몇 라운드에 만났는지 출력
+        print(cnt)
+        break
+    elif n == 1:  # 만약 서로 만나지 않았을때
+        print(-1)
+        break
+    cnt += 1  #만나지 않았다면 다음 라운드로 이동
