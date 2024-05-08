cdccb84c03d676183c9bfa02abcd3651b5d132d1
sangwon02 <sangwon020214@gmail.com>
2024-05-08 14:54:27 +0900

    Solved: BOJ 1620 나는야 포켓몬 마스터 이다솜

---
+++ 1620.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+name = {}  # value에 포켓몬의 이름을 닮을 딕셔너리
+num = {}  # key에 포켓몬의 이름을 닮을 딕셔너리
+for i in range(1, n+1):
+    s = input().strip()
+    name[i] = s
+    num[s] = i
+
+for i in range(m):
+    s = input().strip()
+    if s[0].isalpha():  # 만약 s가 알파벳이라면
+        print(num[s])
+    else:
+        print(name[int(s)])  # 만약 s가 숫자라면
