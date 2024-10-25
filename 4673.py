06b675b1fdb5e7f62d272e1ae88dde0df63501cc
이상원 <sangwon020214@gmail.com>
2024-10-25 22:58:54 +0900

    Solved: BOJ 4673 셀프 넘버

--- 4673.py
+++ 4673.py
@@ -1,15 +1,10 @@
-#꼭 다시 보가
-#4673 셀프넘버
-def d(n):
-    x = 0
-    a = list(str(n))
-    for i in a:
-        x = x + int(i)
-    return n+x
+nums = set(range(1, 10000))
+remove_set = set()  
+for num in nums :
+    for n in str(num):
+        num += int(n)
+    remove_set.add(num)  # 생성자가 있는 수는 remove_set에 넣어서 저장
 
-s_set = set()
-for k in range(1,10000):
-    s_set.add(d(k))
-ans = set(range(1,10000)) - s_set
-for num in sorted(ans):
-    print(num)
+self_nums = nums - remove_set  # 차집합을 구함
+for self_num in sorted(self_nums):  # 정렬 후 출력
+    print(self_num)
