f8bc28561baced32008afbd9e26e9824c233a93a
이상원 <sangwon020214@gmail.com>
2024-11-09 23:55:22 +0900

    Solved: BOJ 17413 단어 뒤집기 2

---
+++ 17413.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+string = input().rstrip()
+word_stack = []
+tag = False
+res = ''
+
+for s in string:
+    if s == " ":
+        while word_stack:
+            res += word_stack.pop()
+        res += s
+
+    elif s == "<":
+        while word_stack:
+            res += word_stack.pop()
+        tag = True
+        res += s
+
+    elif s == ">":
+        tag = False
+        res += s
+
+    elif tag:
+        res += s
+
+    else:
+        word_stack.append(s)
+
+
+while word_stack:
+    res += word_stack.pop()
+    
+print(res)
