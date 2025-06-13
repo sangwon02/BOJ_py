0bbe9b069646cc243b76d89f6cbcb31e8ad04048
이상원 <sangwon020214@gmail.com>
2025-06-13 22:47:25 +0900

    Solved: BOJ 1920 수 찾기

--- 1920.py
+++ 1920.py
@@ -1,13 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input()) 
-a = set(map(int, input().split()))  # n개의 정수 입력 받음
+n = int(input())
+nli = set(map(int, input().split()))  
+# 리스트 보다는 set(집합)를 사용해 탐색시간 단축
 m = int(input())
-num = list(map(int, input().split()))  # m개의 정수 입력 받음
+mli = list(map(int, input().split()))  
+# m의 대한 수들은 그냥 리스트에 저장
 
-for i in num:  # num에 있는 정수를 하나씩 돌아봄
-    if i in a:  # 만약 a안에 있다면
-        print(1)  # 1 출력
-    else:  # 만약 없다면
-        print(0)  # 0 출력
+for i in mli:  # mli에 있는 수들을 돌면서
+    if i in nli:  # 만약 nli에 있다면
+        print(1)  # 1을 출력해주고
+    else:  # 아니면
+        print(0)  # 0을 출력
