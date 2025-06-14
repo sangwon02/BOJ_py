13e7197cc9eacd7a64ede048e478862b8d7c2897
이상원 <sangwon020214@gmail.com>
2025-06-14 23:22:36 +0900

    백준 푼 것

---
+++ 20437.py
@@ -0,0 +1,15 @@
+# 못품....
+
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    w = input()
+    k = int(input())
+    n_max = 0
+    n_low = 0
+    # 딕셔너리로 풀려면 되려나?
+    
+    
