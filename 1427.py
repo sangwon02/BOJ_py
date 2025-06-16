39673210618b6938b965fe5e753b3de8c597652f
이상원 <sangwon020214@gmail.com>
2025-06-16 21:48:30 +0900

    백준 푼 것

--- 1427.py
+++ 1427.py
@@ -1,9 +1,12 @@
-str1 = input()
+import sys
+input = sys.stdin.readline
+
+n = input()  # 정수 n을 문자열로 입력 받고
 li = []
+for i in n:  # 각 자리수의 숫자를 문자형으로 리스트에 넣어준다.
+    li.append(i)
+    
+li.sort(reverse = True) # 내림차순으로 정렬해주고
 
-for i in str1:
-    li.append(int(i))
-
-li.sort(reverse=True)
-for i in li:
-    print(i,end="")
+for i in li:  # 리스트안에 있는 요소들을 출력
+    print(i, end="")
