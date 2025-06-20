3bbcbe16e8a8b1e7da0f8183d04001303d72b6f9
이상원 <sangwon020214@gmail.com>
2025-06-20 12:28:52 +0900

    Solved: BOJ 2798 블랙잭

--- 2798.py
+++ 2798.py
@@ -1,14 +1,19 @@
-x, y = map(int, input().split())#카드의 개수 x과 만들수 있는 최대 y
-li = list(map(int, input().split())) #카드를 입력받음
-num = len(li) #리스트 원소의 수
+import sys
+input = sys.stdin.readline
 
-sum = 0 #카드의 합을 저장할 변수
-for i in range(num): #num만큼 반복
-        for j in range(i + 1, num): #i+1에서 num만큼 반복
-                for k in range(j + 1, num):#j+1에서 num만큼 반복
-                        if li[i] + li[j] + li[k] > y: #li[i], li[j], li[k]합이 y보다 크면 다시 위로 돌아감
-                                continue
-                        else: #li[i], li[j], li[k]합이 y보다 작으면 
-                                sum = max(sum, li[i] + li[j] + li[k]) #sum과 합중에 큰것을 sum에 저장
-#sum 출력
-print(sum)
+n, m = map(int, input().split())
+li = list(map(int, input().split()))
+
+sum = 0
+res = 0  # 정답을 담을 변수
+
+for i in range(n-2):  # 모든 경우의 수 구하고
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            sum = li[i] + li[j] + li[k]  # 3카드의 합을 저장
+            if m == sum: # 만약 모든 경우의 수 구하다가 m과 같은 값이 나오면
+                print(sum)  # 출력 후
+                exit()  # 파일 실행 종료
+            elif m-sum > 0 and m-sum < m-res:  # sum이 m보다 작고 만약 res보다 m에 가깝다면
+                res = sum  # res값을 sum값으로 갱신
+print(res)
