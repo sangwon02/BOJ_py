616b87506262d773397eedbed1b6eec33d4fca53
이상원 <sangwon020214@gmail.com>
2025-06-21 09:22:39 +0900

    백준 푼 것

--- 2839.py
+++ 2839.py
@@ -1,29 +1,41 @@
-# n = int(input()) #정수 입력 받음
-# if n%5==0: #n이 5로 나누어떨어지면
-#     print(n//5) #n나누기5 출력
-# elif (n-3)%5==0 and n>=3:#n-3이 5로 나누어지고 n이 3이상이면
-#     print((n-3)//5+1) #(n-3)//5+1을 출력
-# #더이상 주석 달 필요는 없는듯
-# elif (n-6)%5==0 and n>=6:
-#     print((n-6)//5+2)
-# elif (n-9)%5==0 and n>=9:
-#     print((n-9)//5+3)
-# elif (n-12)%5==0 and n>=12:
-#     print((n-12)//5+4)
-# else:
-#     print(-1)
 import sys
 input = sys.stdin.readline
 
-sugar = int(input())
+# 내가 푼 풀이
+n = int(input())  # 배달해야하는 설탕의 무게
 
-bag = 0
-while sugar >= 0 :
-    if sugar % 5 == 0 : 
-        bag += (sugar // 5) 
-        print(bag)
+num = 0 # 봉지의 개수
+res = 0 # 봉지에 따른 설탕의 무게
+
+while (n-res) > 2:  # 만약 남은 설탕의 무게가 2 이하면 종료
+    # 남은 설탕이 15 미만이면서 3으로 나누어 떨어지는 경우
+    if ((n - res) < 15) and ((n-res)%3 == 0): 
+        num += ((n - res) // 3)  # 3kg필요한 만큼 사용
+        res += n - res  # 남은 설탕의 무게를 res에 다 더해줌
+        
+    elif (n - res) >= 5:  # 만약 5키로를 담을 수 있다면
+        num += 1  # 봉지의 개수 +1
+        res += 5  # 봉지에 담은 설탕의 무게 +5
+        
+    elif (n - res) >= 3:  # 이때는 3킬로 담을 수 있는 경우
+        num += 1
+        res += 3
+        
+if res==n:  # 만약 남김없이 담았다면
+    print(num)  # 사용한 봉지의 개수 출력
+else:  # 남은 설탕이 있다면
+    print(-1)  # -1 출력
+    
+# 다른 풀이!
+n = int(input())
+
+num = 0
+while n >= 0 :
+    if n % 5 == 0 :  # 5의 배수이면
+        num += (n // 5)  # 5로 나눈 몫을 구해야 정수가 됨
+        print(num)
         break
-    sugar -= 3  
-    bag += 1
-else:
+    n -= 3  
+    num += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
+else :
     print(-1)
