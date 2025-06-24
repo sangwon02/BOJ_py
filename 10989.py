4d0e68232436f1e49ede73d0a9c19d6e34799cd1
이상원 <sangwon020214@gmail.com>
2025-06-24 10:38:08 +0900

    Solved: BOJ 10989 수 정렬하기 3

--- 10989.py
+++ 10989.py
@@ -1,12 +1,21 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-num_list = [0] * 10001
+# n 입력
+n = int(input())
 
+# 10001 크기의 리스트 생성
+count = [0] * 10001
+
+# n개의 정수를 리스트의 인덱스로 접근하여 개수 세기
 for _ in range(n):
-    num_list[int(sys.stdin.readline())] += 1
+    num = int(input())
+    count[num] += 1
 
+# n 리스트에 접근해 그 개수만큼 인덱스 출력
 for i in range(10001):
-    if num_list[i] != 0:
-        for j in range(num_list[i]):
+    # count[i]이 1 이상이면
+    if count[i] != 0:
+        # 그 값만큼 출력
+        for j in range(count[i]):
             print(i)
