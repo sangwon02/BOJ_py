8e9a33671c083d5157eb12e9c2d6ceff259a6044
이상원 <sangwon020214@gmail.com>
2025-07-01 11:19:04 +0900

    Solved: BOJ 9012 괄호

--- 9012.py
+++ 9012.py
@@ -1,7 +1,7 @@
 import sys
 input = sys.stdin.readline 
 
-n = int(input())  # 테스트 케이스의 개수 입력
+n = int(input())  # n 입력 받음
 
 for _ in range(n):
     s = input().strip()  # 문자열 입력
@@ -11,6 +11,7 @@ for _ in range(n):
     for char in s:
         if char == '(':  # 여는 괄호인 경우
             stack.append(char)  # 스택에 추가
+            
         elif char == ')':  # 닫는 괄호인 경우
             if stack:  # 스택에 여는 괄호가 있는 경우
                 stack.pop()  # 스택에서 여는 괄호 제거
