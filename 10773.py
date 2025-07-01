4a6a18d288160da6746892edfea1a21da3d59aaf
이상원 <sangwon020214@gmail.com>
2025-07-01 11:19:04 +0900

    Solved: BOJ 10773 제로

--- 10773.py
+++ 10773.py
@@ -1,15 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-k = int(input())
+stack = []  # 리스트를 이용해 스택 생성
 
-stack = []
+k= int(input())  # k입력 받고
 
-for _ in range(k) :
-    n = int(input())
-    if n == 0 :
+for i in range(k):  # k번 정수를 입력 받는다.
+    num = int(input())
+    
+    if num == 0:  # 만약 0이면 가장 최근에 쓴 수를 지우고
         stack.pop()
-    else :
-        stack.append(n)
-
-print(sum(stack))
+    else:  # 0이 아니면
+        stack.append(num)  # 스택의 규칙을 지키면서 stack에 삽입한다.
+        
+print(sum(stack))  # 총합 출력
