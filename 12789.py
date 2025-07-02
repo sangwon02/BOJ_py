839e1051e16cd99b347412f00fa698233bdc12e6
이상원 <sangwon020214@gmail.com>
2025-07-02 09:57:39 +0900

    Solved: BOJ 12789 도키도키 간식드리미

--- 12789.py
+++ 12789.py
@@ -2,18 +2,19 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-students = list(map(int, input().split())) 
+li = list(map(int, input().split()))
 
-stack = []
+ing = []
 now = 1
 
-for student in students:
-    stack.append(student)
-    while stack and stack[-1] == now:
-        stack.pop()
-        now +=1
+for num in li: # 리스트를 돌면서
+    ing.append(num)  # 일단 대기 줄로 보내고
+    
+    while ing and ing[-1] == now:  # 간식을 받을 수 있는 경우(순서가 맞는 경우)
+        ing.pop() # 간식을 받은 사람 제거하고
+        now += 1  # 받을 사람의 번호 +1
 
-if stack: 
-    print('Sad') 
-else:
-    print('Nice')
+if ing:  # 만약 비어있지 않으면
+    print("Sad")
+else:  # 만약 비어있다면
+    print("Nice")
