948b1916733431122bdcd8712a078453e8388388
이상원 <sangwon020214@gmail.com>
2025-07-04 12:19:08 +0900

    Solved: BOJ 2164 카드2

--- 2164.py
+++ 2164.py
@@ -1,14 +1,17 @@
-import sys
 from collections import deque
+import sys
+
 input = sys.stdin.readline
 
-n = int(input())  # 카드 더미 생성
-deq = deque([i for i in range(1,n+1)])
+n = int(input())
+d = deque()  # 덱을 생성
 
-while len(deq) > 1:  # 카드가 하나 남을때 까지 반복
-    deq.popleft()  # 가장 위에 있는 카드 제거
-    # 가장 위에 있는 카드 맨 밑으로 넣어줌
-    card = deq.popleft()
-    deq.append(card)
-# 답 출력
-print(deq[0])
+for i in range(1,n+1):
+    d.append(i)  # 카드 순서대로 만들고
+
+while len(d) != 1:  # d의 크기가 1이면 종료
+    d.popleft()  # 왼쪽에 있는 값 제거하고
+    d.append(d.popleft())  # 왼쪽에 있는 값 오른쪽에 삽입
+    # 반복
+    
+print(d[0])  # 마지막으로 남은 값 출력
