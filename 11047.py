3d9af2e50502d2e8991b71dc54d74f29fa7ae041
이상원 <sangwon020214@gmail.com>
2025-07-22 22:00:07 +0900

    백준 푼 것

--- 11047.py
+++ 11047.py
@@ -1,16 +1,23 @@
-n1, n2 = map(int, input().split())
-li = []
-cnt = 0
+import sys
+input = sys.stdin.readline
 
+n, k = map(int, input().split())
+li = []  # 동전을 담을 리스트
 
-for i in range(n1):
+for _ in range(n):
     li.append(int(input()))
 
-sorted(li)
+li.reverse() # 오름 차순이 아닌 내림차순으로 정렬
+cnt = 0 # 리스트에 있는 동전을 체크할 변수
+res = 0 # k를 만들기 위해 필요한 코인의 수
 
-for j in range(n1-1, -1, -1):
-    cnt = cnt + n2//li[j]
-    n2 = n2%li[j]
+while k != 0:
+    # 큰 코인부터 
+    if k >= li[cnt]: # 현재 탐색중인 코인의 값어치가 남은 돈 이상이면
+        res += k//li[cnt]  # 사용한 코인의 수만큼 +
+        k = k%li[cnt]  # 남은 돈을 나누기한 나머지로 업데이트 해주고
         
-
-print(cnt)
+    else:  # 아니면
+        cnt += 1  # 다음 코인탐색
+        
+print(res) # 답출력
--- test.py
+++ test.py
@@ -1,15 +1,4 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-
-num = 0
-while n >= 0 :
-    if n % 5 == 0 :  # 5의 배수이면
-        num += (n // 5)  # 5로 나눈 몫을 구해야 정수가 됨
-        print(num)
-        break
-    n -= 3  
-    num += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
-else :
-    print(-1)
+print(13//4)
