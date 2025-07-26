2c67d3c2bd2e0de53ad3f3013cd7eb349e00cdfc
이상원 <sangwon020214@gmail.com>
2025-07-27 00:18:13 +0900

    백준 푼 것

--- 11399.py
+++ 11399.py
@@ -2,12 +2,12 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-li = list(map(int, input().split()))
+li = list(map(int, input().split()))  # 사람들 돈 뽑는 시간 받음
 
 li.sort(reverse=False)  # 오름차순으로 정렬
-result = 0
+res = 0
 
 for i in range(n):
-    result += sum(li[:i+1]) # 첫번째 수부터 i+1수를 더한 수를 result에 저장 
+    res += sum(li[:i+1]) # 첫번째 수부터 i+1수를 더한 수를 res에 저장 
 
-print(result)
+print(res)
