fb39259c4afa4b98db77f28be9db37e3295fe3f7
이상원 <sangwon020214@gmail.com>
2025-08-26 19:32:51 +0900

    Solved: BOJ 1010 다리 놓기

--- 1010.py
+++ 1010.py
@@ -1,23 +1,9 @@
-import sys  
+import sys 
+import math 
+input = sys.stdin.readline
 
 t = int(input())
 
 for i in range(t):
-    n, m = map(int, sys.stdin.readline().split())
-    result = m
-    for j in range(n-1):
-        m -= 1
-        result = result * m
-    for j in range(n):
-        result = result / n
-        n -= 1
-    print(int(result))
-
-import math  # math함수들 불러옴
-import sys  
-
-t = int(input())
-
-for i in range(t):
-    n, m = map(int, sys.stdin.readline().split())
+    n, m = map(int, input().split())
     print(math.comb(m, n))  # mCn을 구하는 함수를 사용해 바로 출력
