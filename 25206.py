19538f84061df4b26076fff9c7a53c588da45f47
이상원 <sangwon020214@gmail.com>
2026-03-07 23:56:02 +0900

    Solved: BOJ 25206 너의 평점은

--- 25206.py
+++ 25206.py
@@ -1,5 +1,5 @@
-rating = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
-grade = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+li1 = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+li2 = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
 
 total = 0	
 result = 0	
@@ -10,6 +10,6 @@ for _ in range(20) :
 
     if lank != 'P' :
         total += credit
-        result += credit * grade[rating.index(lank)]
+        result += credit * li2[li1.index(lank)]
 
 print(round((result / total), 6))
