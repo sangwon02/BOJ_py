f9fc84ab2f0826c01a40b783f3cf60ef375491f4
이상원 <sangwon020214@gmail.com>
2026-03-13 16:37:31 +0900

    Solved: BOJ 1904 01타일

--- 1904.py
+++ 1904.py
@@ -2,12 +2,13 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 
-dp = [0] * (n + 1) 
-dp[1] = 1 
+li = [0] * (n + 1) 
+
+li[1] = 1 
+
 if n > 1:
-    dp[2] = 2
-
+    li[2] = 2
 for i in range(3, n+1):
-    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746 
+    li[i] = (li[i - 1] + li[i - 2]) % 15746 
 
-print(dp[n])
+print(li[n])
