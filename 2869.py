f7d4e7514ffa8ce1633f3f2c37e15659332456b4
이상원 <sangwon020214@gmail.com>
2026-03-24 23:13:13 +0900

    Solved: BOJ 2869 달팽이는 올라가고 싶다

--- 2869.py
+++ 2869.py
@@ -1,8 +1,7 @@
-a, b, v= map(int, input().split()) #공백을 구분하여 정수를 3개 입력 받음
-k = (v-b)/(a-b) #남은 거리와 하루에 올라는 거리를 나누어주어 저장함 
+import math
 
-if k == int(k): #k가 정수면
-    print(int(k)) #k만 프린트
-else: #정수가 아니면
-    print(int(k)+1) #k를 반올림 해주어 정수로 출력
- 
+a, b, v= map(int, input().split())
+
+k = (v-b)/(a-b)
+
+print(math.ceil(k))
