97721aac2c8784f1f47b6258ddd0f1fcabcfdff6
이상원 <sangwon020214@gmail.com>
2026-04-19 22:59:01 +0900

    Solved: BOJ 2775 부녀회장이 될테야

--- 2775.py
+++ 2775.py
@@ -1,12 +1,27 @@
-n = int(input()) #정수를 입력 받음
+import sys
+input = sys.stdin.readline
 
-for i in range(n): #n만큼 반복
-    num = [] #리스트 생성
-    a = int(input()) #정수를 입력 받음
-    b = int(input()) #정수를 입력 받음
-    num = [j for j in range(1, b+1)] #리스트에 a부터 b까지 추가
-    
-    for l in range(a): #a만큼 반복
-        for j2 in range(1, b): #1부터 b까지 j2에 대입하면서 반복
-            num[j2] += num[j2-1] #j2에 있던 원소에 j2-1를 더함
-    print(num[b-1]) #b-1자리에 있는 원소 프린트
+# 아파트 문제 (백준 2775)
+# k층 n호에 사는 사람 수 구하기
+# 규칙: k층 n호 = (k-1)층 1호 ~ n호까지의 합
+
+t = int(input())
+
+for _ in range(t):
+    k = int(input())   # 층
+    n = int(input())   # 호
+
+    # dp[층][호] = 그 방에 사는 사람 수
+    dp = [[0] * (n + 1) for _ in range(k + 1)]
+
+    # 0층 초기화: 1호=1명, 2호=2명, 3호=3명 ...
+    for j in range(1, n + 1):
+        dp[0][j] = j
+
+    # 1층부터 k층까지 채우기
+    for i in range(1, k + 1):
+        for j in range(1, n + 1):
+            # i층 j호 = i층 (j-1)호 + (i-1)층 j호
+            dp[i][j] = dp[i][j-1] + dp[i-1][j]
+
+    print(dp[k][n])
